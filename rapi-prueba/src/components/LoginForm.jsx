bd27bf922aca32737aaeb6b6d3fa17ff8eb5867a
EvryDash05 <elpandaeverjoelccencho@gmail.com>
2024-11-02 02:08:10 -0500

    Se agregan los cripts para el login y registro

--- rapi-prueba/src/components/LoginForm.jsx
+++ rapi-prueba/src/components/LoginForm.jsx
@@ -2,16 +2,48 @@
 import React from 'react';
 
 function LoginForm() {
-    const handleLoginSubmit = (e) => {
+
+    const END_POINT_CREATE = "http://localhost:8080/api/v1/auth/login"
+
+    const handleLoginSubmit = async (e) => {
         e.preventDefault();
-        console.log('Iniciando sesión...');
+
+        try {
+            const data = Object.fromEntries(new FormData(e.target));
+            console.log(data);
+
+            const response = await fetch(END_POINT_CREATE, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+
+            if (!response.ok) {
+                console.error("Error al iniciar sesión:", response.statusText);
+                alert("Error al iniciar sesión. Por favor verifica tus datos.");
+                return;
+            }
+
+            const customerData = await response.json();
+            console.log("Customer Data:", customerData);
+
+            localStorage.setItem('customerData', JSON.stringify(customerData));
+
+            alert("Login exitoso");
+        } catch (error) {
+            console.error("Error en la solicitud:", error);
+            alert("Ocurrió un problema al conectar con el servidor.");
+        }
+
     };
 
     return (
         <form className="formulario__login" onSubmit={handleLoginSubmit}>
             <h2>Iniciar sesión</h2>
-            <input type="text" placeholder="Correo Electronico" required />
-            <input type="password" placeholder="Contraseña" required />
+            <input type="text" placeholder="Correo Electronico" name='email' required />
+            <input type="password" placeholder="Contraseña" name='password' required />
             <button type="submit">Entrar</button>
         </form>
     );
